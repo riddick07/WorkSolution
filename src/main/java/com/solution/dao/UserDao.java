88603dc389f5845763e08dc3f7ff8942d5290f68
Dmitry.Karpenko <de.azucar07@gmail.com>
2014-02-22 18:58:43 +0200

    4

--- src/main/java/com/solution/dao/UserDao.java
+++ src/main/java/com/solution/dao/UserDao.java
@@ -4,6 +4,8 @@ import com.solution.dao.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.Map;
 
 public class UserDao {
 
@@ -14,5 +16,17 @@ public class UserDao {
     public User create(User user) {
         em.persist(user);
         return user;
+    }
+
+    public void delete(final Object id) {
+        this.em.remove(this.em.getReference(User.class, id));
+    }
+
+    public User find(final Object id) {
+        return (User) this.em.find(User.class, id);
+    }
+
+    public User update(final User t) {
+        return this.em.merge(t);
     }
 }
